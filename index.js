355b1212ca6e89caff9181856759b94fcf37a450
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-28 18:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ function preventRefreshOnSubmit(){
 function retrieveEmployeeInformation(){
   return document.querySelector('input').value
 }
-function addNewElementAsLi(){
-  console.log(retrieveEmployeeInformation())
-    document.querySelector('ul').append('<li>${retrieveEmployeeInformation()}</li>')
+function addNewElementAsLi() {
+console.log(retrieveEmployeeInformation())
+  document.querySelector('ul').append(`<li>${retrieveEmployeeInformation()}</li>`)
 }
-function addNewLiOnClick(){
-  document.querySelector('input[type=submit]').addEventListener('click', function(){
+
+function addNewLiOnClick() {
+  document.querySelector('input[type=submit]').addEventListener('click', function() {
     addNewElementAsLi()
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function() {
     document.querySelector('ul').innerHTML = ""
   })
 }
