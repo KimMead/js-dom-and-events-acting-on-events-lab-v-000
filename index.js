1b76aeeb63b3af913d62cbf18896b898907056dd
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-28 18:44:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
+
     })
 }
-function retrieveEmployeeInformation(){
+function retrieveEmployeeInformation() {
   return document.querySelector('input').value
+
 }
+
 function addNewElementAsLi() {
 console.log(retrieveEmployeeInformation())
   document.querySelector('ul').append(`<li>${retrieveEmployeeInformation()}</li>`)
 }
+
 function addNewLiOnClick() {
   document.querySelector('input[type=submit]').addEventListener('click', function() {
     addNewElementAsLi()
     document.querySelector('input').value = ""
   })
 }
+
 function clearEmployeeListOnLinkClick() {
   document.querySelector('a').addEventListener('click', function() {
     document.querySelector('ul').innerHTML = ""
